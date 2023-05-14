e440a748a525ee0bcbe741d61fb1fe55eb6b7899
Jack OHare <jackohare27@gmail.com>
2023-05-13 23:19:40 -0400

    Moveable Cube/Drop Down

---
+++ Media/Credits.md
@@ -0,0 +1,2 @@
+Image icons
+<a target="_blank" href="https://icons8.com/icon/MmupZtPbnw66/menu">Menu</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import * as THREE from "three";
-
+//import Stats from "/node_modules/three/examples/jsm/libs/stats.module.js"; use for frames stats
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 // Scene
 const scene = new THREE.Scene();
-
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 // Add a cube to the scene
-const geometry = new THREE.BoxGeometry(3, 1, 3); // width, height, depth
-const material = new THREE.MeshLambertMaterial({ color: 0xfb8e00 });
+const geometry = new THREE.BoxGeometry(3, 3, 3); // width, height, depth
+const material = new THREE.MeshLambertMaterial({ color: 0xffffff });
 const mesh = new THREE.Mesh(geometry, material);
 mesh.position.set(0, 0, 0);
 scene.add(mesh);
@@ -20,23 +21,36 @@ scene.add(directionalLight);
 
 // Camera
 const width = 10;
-const height = width * (window.innerHeight / window.innerWidth);
-const camera = new THREE.OrthographicCamera(
-  width / -2, // left
-  width / 2, // right
-  height / 2, // top
-  height / -2, // bottom
-  1, // near
-  100 // far
-);
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+// Movement
+const controls = new OrbitControls(camera,renderer.domElement);
+controls.addEventListener( 'change', renderer ); // add this only if there is no animation loop (requestAnimationFrame)
+//controls.listenToKeyEvents(window); //use when no animation
+//controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+//controls.dampingFactor = 0.05;
+controls.screenSpacePanning = false;
+controls.enablePan = false;
+controls.enableZoom = false;
+
+
+controls.maxPolarAngle = Math.PI / 2;
+
+//camera initial placement
 camera.position.set(4, 4, 4);
 camera.lookAt(0, 0, 0);
 
+//controls camera placement and updating of screen
+function animate() {
+	requestAnimationFrame( animate );
+    //controls.update();
+	renderer.render( scene, camera );
+}
+
+
 // Renderer
-const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.render(scene, camera);
+animate();
 
 // Add it to HTML
 document.body.appendChild(renderer.domElement);
