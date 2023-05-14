bf8a38a630e7335c34f38ebcdfb986323c83448d
Jack OHare <jackohare27@gmail.com>
2023-05-14 14:45:03 -0400

    Added hover ability(WIP)

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 // Scene
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer({ antialias: true });
+//raycast needs
+const pointer = new THREE.Vector2();
+let INTERSECTED;
+let raycaster;
+
+
 // Add a cube to the scene
 const geometry = new THREE.BoxGeometry(3, 3, 3); // width, height, depth
 const material = new THREE.MeshLambertMaterial({ color: 0xffffff });
@@ -15,7 +21,7 @@ scene.add(mesh);
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+const directionalLight = new THREE.DirectionalLight(0x000000, 0.6);
 directionalLight.position.set(10, 20, 0); // x, y, z
 scene.add(directionalLight);
 
@@ -33,6 +39,11 @@ controls.screenSpacePanning = false;
 controls.enablePan = false;
 controls.enableZoom = false;
 
+//raycast creation
+raycaster = new THREE.Raycaster();
+document.addEventListener('mousemove',onPointerMove)
+
+
 
 controls.maxPolarAngle = Math.PI / 2;
 
@@ -40,10 +51,39 @@ controls.maxPolarAngle = Math.PI / 2;
 camera.position.set(4, 4, 4);
 camera.lookAt(0, 0, 0);
 
+//controls where mouse points
+function onPointerMove( event ) {
+
+	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+}
 //controls camera placement and updating of screen
 function animate() {
 	requestAnimationFrame( animate );
-    //controls.update();
+    
+	camera.updateMatrixWorld();
+	//raycast pointing
+	raycaster.setFromCamera(pointer, camera);
+	//checks if something intersects it (any object)
+	const intersects = raycaster.intersectObjects(scene.children,false);
+	if(intersects.length > 0)
+	{
+		//if the name of nothing does not equal nothing
+		if(INTERSECTED != intersects[0].object)
+		{
+			if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+
+			INTERSECTED = intersects[ 0 ].object;
+			INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
+			INTERSECTED.material.emissive.setHex( 0xffffff );	
+		}
+	} else //if not there, turn back to normal hue
+	{
+		if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+
+		INTERSECTED = null;
+	}
 	renderer.render( scene, camera );
 }
 
